2fe5af8d1e479bdf20b2184ac4c54f8f525029a5
Marcin Kaleka <marcin.kaletka@gmail.com>
2020-02-18 15:18:04 +0100

    main logic

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 #!/usr/bin/python3
 
-def main():
-    print("test")
+import yaml
+import os
+import argparse
 
-if __name__ == "__main__":
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_file", help="yaml config location")     
+    parser.add_argument("server_name", help="server name to connect")
+    args = parser.parse_args()
+    
+    with open(args.config_file, 'r') as f:
+        config = yaml.load(f)
+  
+    try:
+      ip = config[args.server_name]["ip"]
+
+    except KeyError:
+      print(f"Server {args.server_name} not found in config file")
+
+    else:
+      print(config[args.server_name])
+      os.system(f"ssh {ip}")
+
+if __name__ == "__main__":    
     main()
